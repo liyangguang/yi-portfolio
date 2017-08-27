521e51d2e4b77f200c8227bbaa315ec1a57597a1
Yangguang Li <liyangguangcn@gmail.com>
2017-08-27 10:19:57 -0700

    responsive. imager viewer

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -3,8 +3,6 @@
 console.log('Hi, I\'m Yi Nie. Welcome to my portfolio site!');
 
 $(function () {
-  var animationEndState = [{ x: -110, y: -200, scale: 3, rotate: 80 }, { x: 140, y: -200, scale: 3, rotate: 60 }, { x: -200, y: 300, scale: 3, rotate: -60 }, { x: 110, y: 200, scale: 3, rotate: 0 }, { x: -510, y: -300, scale: 2, rotate: 0 }, { x: -310, y: 0, scale: 2, rotate: 80 }, { x: 710, y: 200, scale: 2, rotate: -100 }, { x: 310, y: -100, scale: 2, rotate: -100 }];
-
   $(window).scroll(function () {
     var position = $(document).scrollTop();
 
@@ -42,5 +40,24 @@ $(function () {
     $('#competitor .tab-content').hide();
     $('#competitor .tab-content-' + $(this).attr('data-tab')).show();
   });
+
+  // prototype video view
+  $('#side-page .mask').click(function (e) {
+    var videoURL = $(e.target).parent().find('source').attr('src');
+    window.open(videoURL, '_blank');
+  });
+
+  // images view
+  $('#side-page .side-project img').click(function (e) {
+    var imageURL = $(e.target).attr('src');
+    $('.image-preview-container .image').css('background-image', 'url(' + imageURL + ')');
+    console.log(imageURL, $('.image-preview-container .image'));
+    $('.image-preview-container').fadeIn(500);
+  });
+  $('.image-preview-container').click(function (e) {
+    $('.image-preview-container').fadeOut(500);
+  });
 });
+
+var animationEndState = [{ x: -110, y: -200, scale: 3, rotate: 80 }, { x: 140, y: -200, scale: 3, rotate: 60 }, { x: -200, y: 300, scale: 3, rotate: -60 }, { x: 110, y: 200, scale: 3, rotate: 0 }, { x: -510, y: -300, scale: 2, rotate: 0 }, { x: -310, y: 0, scale: 2, rotate: 80 }, { x: 710, y: 200, scale: 2, rotate: -100 }, { x: 310, y: -100, scale: 2, rotate: -100 }];
 //# sourceMappingURL=scripts.js.map
